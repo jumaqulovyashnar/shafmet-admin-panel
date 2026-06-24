a620bbf93cb21ed36239b83333e36249af4b50c2
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-06-25 00:01:07 +0500

    add

--- src/api/constant/images.ts
+++ src/api/constant/images.ts
@@ -1,7 +1,13 @@
 import LoginPageImage from "@/assets/images/LoginPageImage.svg"
 import loginPageLogo from "@/assets/images/loginPageLogo.svg"
+import profile1 from "@/assets/images/profile1.svg"
+import profile2 from "@/assets/images/profile2.svg"
+import profile3 from "@/assets/images/profile3.svg"
 
 export const images = {
   loginPageLogo,
   LoginPageImage,
+  profile1,
+  profile2,
+  profile3,
 }
