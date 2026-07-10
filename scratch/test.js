b09b46bc0d6ad543e20080dcd7287a0914aab934
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-07-10 21:46:50 +0500

    add

--- scratch/test.js
+++ scratch/test.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 async function test() {
   try {
-    // Let's read the .env file to get the token or just fetch public info,
-    // wait we can read userStore from sessionStorage or just run a query.
     console.log("Fetching workers...");
   } catch (err) {
     console.error(err);
